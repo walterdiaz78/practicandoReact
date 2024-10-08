2ad3e4a06c712114eb6a28b64a5f60a6bd46032e
walterdiaz78 <walterdiaz78@gmail.com>
2024-10-08 20:22:08 -0300

    Primer commit practicando React

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Practicando React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Editá <code>src/App.jsx</code> y guardá para probar HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click en el logo de Vite o React para mas informacion
+      </p>
+    </>
+  )
+}
+
+export default App
